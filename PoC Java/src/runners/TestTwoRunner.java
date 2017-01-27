d4e2b0552466285432e481f78918a722c9298d6e
TRUEXTEND\walter.ramirez <walter.ramirez@mindjet.com>
2017-01-27 18:27:50 -0400

    - Replacing JUnit by TestNG - Included Runners and working on Parallel Executions

---
+++ PoC Java/src/runners/TestTwoRunner.java
@@ -0,0 +1,27 @@
+package runners;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import features.pages.DriverManager;
+
+/*
+ * 1/27/2017 - Walter
+ *     - Updated File Name
+ *     - Included initialization of Driver on Runner because the Variable is being sent from testng.xml
+ */
+@CucumberOptions(
+		features = "src/features/FailedTest.feature",		
+		glue = "features.steps",
+		dryRun = false
+		)
+public class TestTwoRunner extends AbstractTestNGCucumberTests {
+
+	@Parameters("browser")
+	@BeforeClass
+	public void startDriver(String browser){
+		DriverManager.initialize(browser);
+	}
+}
